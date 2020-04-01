220f0c481def72388aa311e6607d6b4a6ee123cb
worthyontheweb <claire@worthyontheweb.co.uk>
2020-04-01 01:56:53 +0100

    updated description

--- helloWorthy.php
+++ helloWorthy.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * @package Hello_Dolly
- * @version 1.6
+ * @version 1.0
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.6
-Author URI: http://ma.tt/
+Plugin Name: Hello Worthy
+Plugin URI: http://wordpress.org/extend/plugins/hello-worthy/
+Description: This is based on the Hello Dolly plugin that comes as a standard install of all WordPress installations. Instead of lyrics to the song Hello, Dolly, this plugin will display a quote, reminder or other comment.
+Author: Claire Worthington
+Version: 1.0
+Author URI: http://worthyontheweb.co.uk
 */
 
 function hello_dolly_get_lyric() {
